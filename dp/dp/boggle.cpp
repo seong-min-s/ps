792dbaecc74441568f3db17a96085909dac95bcf
최성민 <csm5588@gmail.com>
2020-01-30 09:26:21 +0900

    메모이제이션 적용안한 boggle

--- dp/dp/boggle.cpp
+++ dp/dp/boggle.cpp
@@ -4,44 +4,62 @@
 using namespace std;
 
 int dirX[8] = { 1, 1, 0, -1, -1, -1, -1,  0 };
+int dirY[8] = { 0, 1, 1,  1,  0, -1, -1, -1 };
 
-int diry[8] = { 0, 1, 1,  1,  0, -1, -1, -1 };
+char board[5][5];
 
-bool FindWordRecursive(int index, int lastIndex int x, int y)
+bool inRange(int y, int x)
 {
-	bool isWord;
+	if (y < 0 || x < 0 || y > 4 || x > 4)
+	{
+		return false;
+	}
+	return true;
+}
 
-	if (
-		if (index > lastIndex)
+bool FindWordRecursive(int y, int x, const string& word)
+{
+	if (!inRange(y, x))
+	{
+		return false;
+	}
+	if (board[y][x] != word[0])
+	{
+		return false;
+	}
+	if (word.size() == 1)
+	{
+		return true;
+	}
+
+	for (int direction = 0; direction < 8; ++direction)
+	{
+		int nextY = y + dirY[direction], nextX = x + dirX[direction];
+
+		if (FindWordRecursive(nextY, nextX, word.substr(1)))
 		{
 			return true;
 		}
-		else
-		{
-			index++;
-				for (int i = 0; i < 8; i++)
-				{
-					isWord = FindWordRecursive(index,
-
-				}
-
-		}
-
+	}
+	return false;
 }
+
 int main(void)
 {
-
 	int testCase = 0;
-	char word[10];
-	char board[5][5];
-	int lastIndex = 0;
+	int num = 0;
+	char cstr[] = "hello";
+	string word = "PREETY";
 	bool isWord = false;
 
+	FILE* fp = fopen("test.txt", "r");
+	//fscanf_s(fp, board);
+	
 	cin >> testCase;
 
 	for (int i = 0; i < testCase; i++)
 	{
-		//보드판
+		//보드판입력받기
 		for (int y = 0; y < 5; y++)
 		{
 			for (int x = 0; x < 5; x++)
@@ -49,32 +67,28 @@ int main(void)
 				cin >> board[y][x];
 			}
 		}
-
-		//단어
-		cin >> word;
-
-		//lastIndex = word.l
-
-		for (int y = 0; y < 5; y++)
+		//cin >> num;
+		num = 1;
+		for (int i = 0; i < num; i++)
 		{
-			for (int x = 0; x < 5; x++)
+			//검색할단어
+			//cin >> word;
+			//word = "PREETY";
+			isWord = false;
+			for (int y = 0; y < 5; y++)
 			{
-				if (word[0] == board[y][x])
+				for (int x = 0; x < 5; x++)
 				{
-					isWord = FindWordRecursive(0, y, x);
+					isWord = FindWordRecursive(y, x, word);
+					if (isWord)
+						break;
 				}
-
-				if (isWord == true)
-				{
+				if (isWord)
 					break;
-				}
 			}
+			//결과 출력
+			cout << word << " " << isWord << endl;
 		}
-		//결과 출력
-		cout << word << " " << isWord << endl;
-
 	}
-
-
 	return 0;
 }
