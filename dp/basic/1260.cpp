7158e02ca4debded8baf9965441a596d590ae43b
최성민 <csm5588@gmail.com>
2020-02-03 13:09:59 +0900

    성공
    2124KB, 4ms, 1245B

--- dp/basic/1260.cpp
+++ dp/basic/1260.cpp
@@ -1,49 +1,47 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<algorithm>
 using namespace std;
 
 vector<bool> isVisited;
 vector<int> vs;
 vector<vector<int>> graph;
 queue<int> q;
+
 void dfs(int vertex)
 {
-	cout << vertex << " ";
-	isVisited[vertex] = 1;
-	for (int i = 1; i < isVisited.size(); i++)
+	if (isVisited[vertex] == 1)
 	{
-		if (graph[vertex][i] == 1 && isVisited[i] != 1)
-		{
-			dfs(i);
-		}
-	}
-}
-void bfs()
-{
-	int vertex;
-	if (!q.empty())
-	{
-		vertex = q.front();
-		if (isVisited[vertex] == 1)
-		{
-			return;
-		}
-		cout <<vertex << " ";
-		q.pop();
-	}
-	else {
 		return;
 	}
-	isVisited[vertex] = 1;
-	for (int i = 0; i < isVisited.size(); i++)
+	cout << vertex << " ";
+	isVisited[vertex] = true;
+
+	for (int i = 0; i < graph[vertex].size(); i++)
 	{
-		if (graph[vertex][i] == 1 && isVisited[i] != 1)
+		dfs(graph[vertex][i]);
+	}
+}
+void bfs(int vertex)
+{
+	isVisited[vertex] = true;
+	q.push(vertex);
+
+	while (!q.empty())
+	{
+		int value = q.front();
+		q.pop();
+		cout << value << " ";
+		for (int i = 0; i < graph[value].size(); i++)
 		{
-			q.push(i);
+			if (!isVisited[graph[value][i]])
+			{
+				q.push(graph[value][i]);
+				isVisited[graph[value][i]] = true;
+			}
 		}
 	}
-	bfs();
 }
 
 int main()
@@ -54,21 +52,24 @@ int main()
 	int from, to;
 
 	cin >> vertex >> edge >> startVertex;
-	vs.assign(vertex + 1, 0);
-	graph.assign(vertex + 1, vs);
-	isVisited.assign(vertex + 1, 0);
-	q.push(startVertex);
+	graph.assign((vertex + 1), vs);
+	isVisited.assign((vertex + 1), 0);
+
 	for (int i = 0; i < edge; i++)
 	{
 		cin >> from >> to;
-		graph[from][to] = 1;
-		graph[to][from] = 1;
+		graph[from].push_back(to);
+		graph[to].push_back(from);
+	}
+	for (int i = 0; i < graph.size(); i++)
+	{
+		sort(graph[i].begin(), graph[i].end());
 	}
 	dfs(startVertex);
 	isVisited.erase(isVisited.begin(), isVisited.end());
-	isVisited.assign(vertex + 1, 0);
+	isVisited.assign((vertex + 1), 0);
 	cout << endl;
-	bfs();
+	bfs(startVertex);
 
 	return 0;
 }
