9d15d1400cae4b6e6ba61e4cb5c477972ec6d377
최성민 <csm5588@gmail.com>
2020-02-04 10:54:17 +0900

    시간초과 해결
    메모이제이션 적용
    +)점화식으로도 해결할 수 있음.

--- dp/dp/9095.cpp
+++ dp/dp/9095.cpp
@@ -1,31 +1,33 @@
 /*1+2+3¥ı«œ±‚<baekjoon>*/
 #include<cstdio>
-int substractRecursive(int subject, int factor)
+int sum[12] = { 0 };
+int substractRecursive(int subject)
 {
-	int sum = 0;
-	subject = subject - factor;
 	if (subject == 0)
-	{
 		return 1;
-	}
 	else if (subject < 0)
-	{
 		return 0;
-	}
-	sum = substractRecursive(subject, 1) + substractRecursive(subject, 2) + substractRecursive(subject, 3);
+
+	int& ret = sum[subject];
+
+	if (ret != 0)
+		return ret;
+
+	ret = substractRecursive(subject - 1) + substractRecursive(subject - 2) + substractRecursive(subject - 3);
+	return ret;
 }
 int main(void)
 {
 	int T = 0;
 	int num = 0;
-	int result = 0;
+	int result[12] = { 0 };
 	scanf_s("%d", &T);
 
 	for (int i = 0; i < T; i++)
 	{
 		scanf_s("%d", &num);
-		result = substractRecursive(num, 0);
-		printf("%d\n", result);
+		result[i] = substractRecursive(num);
+		printf("%d\n", result[i]);
 	}
 	return 0;
 }
