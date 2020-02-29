1c3036e07051b6e79f4a3cfc68f4d3f08c787d80
최성민 <csm5588@gmail.com>
2020-02-29 09:11:01 +0900

    time out

--- dp/samsung/1767_1.cpp
+++ dp/samsung/1767_1.cpp
@@ -2,6 +2,11 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+
+typedef struct connectInfo {
+	bool bIsConnected;
+	int len;
+}ConnectInfos;
 int dir[4][2]
 {
 	{-1,0},
@@ -10,56 +15,51 @@ int dir[4][2]
 	{0,-1},
 };
 
-int getMinLen(int yPos,int xPos, vector<vector<int>>& cells)
+ConnectInfos set(int dIdx, int yPos, int xPos, vector<vector<int>>& cells)
 {
-	int optimumDir= -1;
-	int minLen=1234567;
-	//4개의 방향을 돌면서 전원부가 연결되었을 때의 최소길이를 구한다. 전원부가 연결되지 않으면 최적 방향은 -1이다.
-	for (int dIdx = 0; dIdx < 4; dIdx++)
-	{
-		int len = 0;
-		   
-		int y=yPos+ dir[dIdx][0];
-		int x=xPos+ dir[dIdx][1];
+	ConnectInfos aInfo;
 
-		while (x >= 0 && x < cells.size() && y >= 0 && y < cells.size() && !cells[y][x])
+	aInfo.bIsConnected = false;
+	aInfo.len = 0;
+
+	int y = yPos + dir[dIdx][0];
+	int x = xPos + dir[dIdx][1];
+
+	while (y >= 0 && y < cells.size() && x >= 0 && x < cells.size() && !cells[y][x])
+	{
+
+		if (y == 0 || y == cells.size() - 1 || x == 0 || x == cells.size() - 1)
 		{
-			len++;
+			aInfo.bIsConnected = true;
+		}
+		y += dir[dIdx][0];
+		x += dir[dIdx][1];
+	}
+	if (!aInfo.bIsConnected)
+	{
+		return aInfo;
+	}
+	else
+	{
+		aInfo.bIsConnected = true;
+		
+		y = yPos + dir[dIdx][0];
+		x = xPos + dir[dIdx][1];
+
+		while (y >= 0 && y < cells.size() && x >= 0 && x < cells.size() && !cells[y][x])
+		{
+			cells[y][x] = 2;
+			aInfo.len++;
 			y += dir[dIdx][0];
 			x += dir[dIdx][1];
 		}
-		
-		if (x < 0 || x == cells.size() || y < 0 || y == cells.size())//전원부연결되었을 때
-		{
-			if (minLen > len)
-			{
-				optimumDir = dIdx;
-				minLen = len;
-			}
-		}
-	}
-	cells[yPos][xPos] = 3;//최적화끝난 셀 표시
-	if (optimumDir == -1)
-	{
-		return 0;
-	}
-	else//최적방향이 정해졌을 경우 길이를 연결해준다. 코어와 구분하기 위해서 값 2를 넣어준다.
-	{
-		yPos += dir[optimumDir][0];
-		xPos += dir[optimumDir][1];
 
-		while (xPos >= 0 && xPos < cells.size() && yPos >= 0 && yPos < cells.size())
-		{
-			cells[yPos][xPos] = 2;
-			yPos += dir[optimumDir][0];
-			xPos += dir[optimumDir][1];
-		}
+		return aInfo;
 	}
-	return minLen;
 }
 
 int getMaxConnectedLenRecursive(vector<vector<int>>& cells)
-{	
+{
 	bool state = true;
 	int xPos;
 	int yPos;
@@ -81,12 +81,21 @@ int getMaxConnectedLenRecursive(vector<vector<int>>& cells)
 			break;
 		}
 	}
-	
+
 	if (state)
 	{
 		return 0;
 	}
-	//연결
+	for (int dIdx = 0; dIdx < 4; dIdx++)
+	{
+		//연결
+		ConnectInfos aInfo = set(dIdx, yPos, xPos, cells);
+		//재귀
+		getMaxConnectedLenRecursive(cells);
+		//비교
+
+		//해제
+	}
 	return getMinLen(yPos, xPos, cells) + getMaxConnectedLenRecursive(cells);
 }
 int main(int argc, char** argv)
@@ -100,7 +109,7 @@ int main(int argc, char** argv)
 		cin >> size;
 		int sum = 0;
 		vector<int> vs(size, 0);
-		vector<vector<int>> MexinosCells(size,vs);
+		vector<vector<int>> MexinosCells(size, vs);
 		for (int i = 0; i < size; i++)
 		{
 			for (int j = 0; j < size; j++)
